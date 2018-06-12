c185a821d52e6afed30246cfab9716f727e10613
Ranjit V S <RanjitVS@users.noreply.github.com>
2018-06-12 19:59:36 +0530

    Add files via upload

---
+++ relay_testing.ino
@@ -0,0 +1,15 @@
+#define relay 3
+void setup() 
+{
+ pinMode(relay,OUTPUT); // put your setup code here, to run once:
+
+}
+
+void loop()
+{
+ //  put your main code here, to run repeatedly:
+digitalWrite(relay,HIGH);
+delay(5000);
+digitalWrite(relay,LOW);
+delay(5000);
+}
