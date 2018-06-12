b6f79b1398dfabf70de2df72794241b6f56954c9
Ranjit V S <RanjitVS@users.noreply.github.com>
2018-06-12 20:00:59 +0530

    Add files via upload

---
+++ bluetooth_test.ino
@@ -0,0 +1,35 @@
+ #include <SoftwareSerial.h>
+ #define relay 13
+SoftwareSerial BT(10, 11); //TX, RX  pins of arduino respetively
+String command;
+
+void setup()
+{
+ BT.begin(9600);
+ Serial.begin(9600);
+  
+}
+
+void loop() {
+  while (BT.available()){  //Check if there is an available byte to read
+  //delay(10); //Delay added to make thing stable
+  char c = BT.read(); //Conduct a serial read
+  command += c; //build the string.
+  } 
+
+  if (command.length() > 0) {
+    Serial.println(command);
+   if((command.length()>=1)&&(digitalRead(relay)==LOW))   
+   {
+    digitalWrite(relay,HIGH);
+   }  
+   else if((command.length()>=1)&&(digitalRead(relay)==HIGH))
+   {
+    digitalWrite(relay,LOW);
+   }
+  
+ command="";}} //Reset the variable
+//you can add other command to control addition devices by adding an elseif 
+//and the additions commands you add in sketch can be given through voice regonisation as i have created the app buttons only to control three devices
+
+
---
+++ motor_trial.ino
@@ -0,0 +1,58 @@
+/*created by Mostafa Zaghloul with a helper library called RedBot.h you can find 
+ * it on this link https://github.com/sparkfun/RedBot 
+ *--------------------------- thanks a million --------------------------------- 
+ *you can find me on 
+ *FaceBook::https://www.facebook.com/mostafa.zaghlol3
+ *Twitter::https://twitter.com/Mostafazaghlul
+ *to Contact for hardware or software questions 
+ */
+//#include <RedBot.h>
+
+//RedBotSensor left_sen = RedBotSensor(A3);   
+//RedBotSensor center_sen = RedBotSensor(A4); 
+//RedBotSensor right_sen = RedBotSensor(A5);  
+//data_sheet
+// constants that are used in the code. lineStandard is the level to detect 
+// if the sensor is on the line or not. If the sensor value is greater than this
+// the sensor is above a DARK line.
+//
+// SPEED sets the nominal speed
+//motor A
+#define en1 9
+#define in1 4
+#define in2 5
+//motor b
+#define en2 3
+#define in3 6
+#define in4 7
+
+
+
+void setup()
+{ pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  pinMode(en1, OUTPUT);
+  pinMode(en2, OUTPUT);
+ }
+
+void loop()
+{
+  
+  Serial.print("\t");  
+      forward();
+}
+
+
+
+void forward() {
+  analogWrite(en1, 255);
+  analogWrite(en2, 255);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, HIGH);
+}
+
+
